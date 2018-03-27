97159a5c06ef32a582078928da9d72b98720b122
Robert Spari <robert.speri@bearingpoint.com>
2018-03-27 15:34:58 +0200

    Added some styling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 
-import GameSelection from '../GameSelection'
+import GameSelection from '../GameSetup/GameSelection'
 import FourOhFour from '../FourOhFour'
 
 import './style.css'
 
 const App = () => (
-  <div className="appFrame">
-    <Switch>
-      <Route exact path="/" component={GameSelection}/>
-      <Route path="*" component={FourOhFour}/>
-    </Switch>
-  </div>
+    <div className="appFrame">
+        <Switch>
+            <Route exact path="/" component={GameSelection}/>
+            <Route path="*" component={FourOhFour}/>
+        </Switch>
+    </div>
 )
 
 export default App
--- src/components/GameSetup/GameSelection/index.js
+++ src/components/GameSetup/GameSelection/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { push } from 'react-router-redux'
-import { Button } from '../../shared/index'
+import {connect} from 'react-redux'
+import {push} from 'react-router-redux'
+import {Button} from '../../shared/index'
 import './style.css'
 
 const GameSelection = ({push}) =>
-  (
-    <div>
-      <div className={`gameSelect`}>
-        <Button big text="x01" action={() => push('/x01')}/>
-        <Button big text="cricket" action={() => push('/cricket')}/>
-      </div>
-    </div>
-  )
+    (
+        <div className={`gameSelect`}>
+            <h1 className={`mainHeader`}>Choose your Game!</h1>
+            <div className={`gameOptions`}>
+                <Button big text="x01" action={() => push('/x01')}/>
+                <Button big text="cricket" action={() => push('/cricket')}/>
+            </div>
+        </div>
+    )
 
 const mapStateToProps = (state, ownProps = {}) => ownProps
 
